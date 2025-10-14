b97ce12893c64764f4954f14c0f5704bd1ea15d5
WangZiming <wangziming233@emails.ucas.ac.cn>
2025-10-14 03:17:04 +0000

    load_task_img of task3

--- kernel/loader/loader.c
+++ kernel/loader/loader.c
@@ -10,6 +10,20 @@ uint64_t load_task_img(int taskid)
      * 1. [p1-task3] load task from image via task id, and return its entrypoint
      * 2. [p1-task4] load task via task name, thus the arg should be 'char *taskname'
      */
+	if(taskid < 0 || taskid >= tasknum){
+		bios_putstr("Invalid taskid");
+		return 0;
+	}
+	short kernel_sectors = 15;
+	short 
+	int block_id = 1 + kernel_sectors + taskid * 15;
+	uintptr_t mem_addr = 0x52000000;
+	bios_putstr("loading task ");
+	bios_putstr('0' + taskid);
+
+	bios_id_read(mem_addr,15,blockid);
+
+	bios_putstr("done.");
 
     return 0;
 }
