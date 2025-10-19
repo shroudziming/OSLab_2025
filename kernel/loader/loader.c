b48e82f1fc5301d6d909a3a64d4764e46c6f13ad
WangZiming <wangziming233@emails.ucas.ac.cn>
2025-10-19 16:54:38 +0000

    load tasks by name

--- kernel/loader/loader.c
+++ kernel/loader/loader.c
@@ -6,6 +6,7 @@
 #define APP_BASE 0x52000000
 #define APP_ADDR_INTERVAL 0x10000
 #define TASKNUM_ADDR 0x502001fe
+#define KERNEL_SECTORS_ADDR 0x502001fc
 
 /*static void bios_puthex(unsigned char x) {
     const char hex_table[] = "0123456789ABCDEF";
@@ -16,14 +17,26 @@
     bios_putstr(buf);
 }
 */
+short tasknum = *(short *)TASKNUM_ADDR;
+short kernel_sectors = *(short *)KERNEL_SECTORS_ADDR;
+static int find_task_index(char *name){
+	int i;
+	for(i=0;i<tasknum;++i){
+		if(strcmp(tasks[i].name,name) == 0)
+			return i;
+	}
+	return -1;
+}
 
-uint64_t load_task_img(int taskid)
+uint64_t load_task_img(char *taskname)
 {
     /**
      * TODO:
      * 1. [p1-task3] load task from image via task id, and return its entrypoint
      * 2. [p1-task4] load task via task name, thus the arg should be 'char *taskname'
      */
+	//[p1-task3] load by id
+	/*
 	short tasknum = *(short *)TASKNUM_ADDR;
 	if(taskid < 0 || taskid >= tasknum){
 		bios_putstr("Invalid taskid");
@@ -44,6 +57,36 @@ uint64_t load_task_img(int taskid)
 	bios_puthex(p[3]);
 	bios_putstr("\n");
 	*/
+	*/
+	
+	//[p1-task4] load by name
+	
+	int idx = find_task_index(taskname);
+	if(idx < 0){
+		bios_putstr("[Error] Task not found.\n");
+		return 0;
+	}
+	bios_putstr("loading task ");
+	bios_putstr(tasks[idx].name);
+	bios_putstr("\n");
+
+	//bytes offset
+	uintptr_t mem_addr = APP_BASE + idx * APP_ADDR_INTERVAL;
+	uint64_t offset = tasks[idx].file_off;
+	uint64_t size = tasks[idx].file_size;
+
+	uint64_t start_sector = offset / SECTOR_SIZE;
+	uint64_t sector_offset = offset % SECTOR_SIZE;
+	uint64_t end_sector = (offset + size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+	uint64_t num_sectors = end_sector - start-sector;
+
+	static uint8_t sector_buf[SECTOR_SIZE * 15];
+
+	bios_sd_read((uintptr_t)sector_buf,num_sectors,1 + kernel_sectors + start_sector);
+	
+	memcpy((void *)mem_addr,sector_buf + sector_offset,size);
+
+	
 	bios_putstr("done.\n");
 
     return 0;
