14d2941f31c149c99acdac65c49723b6a5b2090d
WangZiming <wangziming233@emails.ucas.ac.cn>
2025-10-19 04:07:39 +0000

    delete codes for debugging

--- kernel/loader/loader.c
+++ kernel/loader/loader.c
@@ -7,6 +7,16 @@
 #define APP_ADDR_INTERVAL 0x10000
 #define TASKNUM_ADDR 0x502001fe
 
+/*static void bios_puthex(unsigned char x) {
+    const char hex_table[] = "0123456789ABCDEF";
+    char buf[3];
+    buf[0] = hex_table[(x >> 4) & 0xF];
+    buf[1] = hex_table[x & 0xF];
+    buf[2] = '\0';
+    bios_putstr(buf);
+}
+*/
+
 uint64_t load_task_img(int taskid)
 {
     /**
@@ -26,8 +36,15 @@ uint64_t load_task_img(int taskid)
 	bios_putchar('0' + taskid);
 
 	bios_sd_read(mem_addr,15,block_id);
-
-	bios_putstr("done.");
+	/*unsigned char *p = (unsigned char *)mem_addr;
+	bios_putstr("First 4 bytes: ");
+	bios_puthex(p[0]);
+	bios_puthex(p[1]);
+	bios_puthex(p[2]);
+	bios_puthex(p[3]);
+	bios_putstr("\n");
+	*/
+	bios_putstr("done.\n");
 
     return 0;
 }
