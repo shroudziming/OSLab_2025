ba45cc1a8114404e735cfa656cdc7b17256a1619
WangZiming <wangziming233@emails.ucas.ac.cn>
2025-12-09 17:08:47 +0000

    pthread.c created

---
+++ tiny_libc/pthread.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+
+/* TODO:[P4-task4] pthread_create/wait */
+void pthread_create(pthread_t *thread,
+                   void (*start_routine)(void*),
+                   void *arg)
+{
+    
+}
+
+int pthread_join(pthread_t thread)
+{
+    /* TODO: [p4-task4] implement pthread_join */
+    return sys_waitpid(thread);
+}
